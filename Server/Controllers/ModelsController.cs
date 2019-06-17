97e768bd6d7c5c7af96ce17099026d79a9af88bd
Илья Иджян <kloder3@gmail.com>
2019-06-17 14:57:46 +0300

    get by odata test

--- Server/Controllers/ModelsController.cs
+++ Server/Controllers/ModelsController.cs
@@ -7,6 +7,9 @@ using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using WebApiLogic;
 
@@ -28,6 +31,31 @@ namespace WebApi.Controllers
             this.logic = logic;
             this.cache = memoryCache;
         }
+
+        [ResponseCache(Location = ResponseCacheLocation.Client, Duration = 600)]
+        [Route("odataprograms")]
+        public JToken GetODataPrograms()
+        {
+            JToken value;
+
+            using (var client = new HttpClient())
+            {
+
+                var urll = @"http://217.16.28.213/1CDB_FPA_DPO/odata/standard.odata/Catalog_%D0%A6%D0%B8%D0%BA%D0%BB%D1%8B%D0%94%D0%9F%D0%9E?$format=json&$select=Ref_Key, ПолноеНаименование, Статус, ГруппаПрограммыОбучения_Key, ФормаОбучения_Key&$filter=Статус eq 'Активный'";
+
+                var authValue = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"Kloder:Kaligula2")));
+                client.DefaultRequestHeaders.Authorization = authValue;
+
+                var response = client.GetAsync(urll).Result;
+                var result = response.Content.ReadAsAsync<JObject>().Result;
+
+                value = result.GetValue("value");
+            }
+
+            return value;
+        }
+
+
 
         [ResponseCache( Location = ResponseCacheLocation.Client, Duration = 30 )]
         [Route("programs")]
